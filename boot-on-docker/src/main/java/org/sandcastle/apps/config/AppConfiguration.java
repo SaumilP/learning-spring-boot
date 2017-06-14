acc924ae9e8fedb7458913ac472763681633c52c
Saumil Patel <saumilpatel1@gmail.com>
2017-06-15 00:45:15 +0200

    boot application running on docker container example added

---
+++ boot-on-docker/src/main/java/org/sandcastle/apps/config/AppConfiguration.java
@@ -0,0 +1,12 @@
+package org.sandcastle.apps.config;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan(value = "org.sandcastle.apps")
+public class AppConfiguration {
+
+}
